ab0d81772272f7d31fedd1268961a41212fafc5b
Matias Saastamoinen <matias.saastamoinen@eng.tamk.fi>
2016-09-12 19:01:55 +0300

    Add project files.

---
+++ EX1/EX1.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		EX1.ino
+ Created:	9/12/2016 7:01:49 PM
+ Author:	ernomasa
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
