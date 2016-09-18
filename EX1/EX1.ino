f41e33000d59f4ed142db63b57e425b24ac41cf7
Matias Saastamoinen <matias.saastamoinen@eng.tamk.fi>
2016-09-18 14:08:17 +0300

    Palautus

--- EX1/EX1.ino
+++ EX1/EX1.ino
@@ -9,41 +9,35 @@ unsigned long sosTime = 0;
 bool sosLedHigh = false;
 unsigned int sosIndex = 0;
 
-
-
 unsigned int sos[] = { dot, dot, dot, dash, dash, dash, dot, dot, dot };
 
-void blingPattern(unsigned int pattern[], unsigned int length, unsigned long &timeOld, unsigned int &patternIndex);
+void blingPattern(unsigned int pattern[], unsigned int length, unsigned long &timeOld, unsigned int &patternIndex, bool &ledHigh);
 void ledControl(uint8_t status);
 
 void setup()
 {
 	// D-register pin 3 to output = arduino nano pin D3
-	//DDRD = DDRD | 0b00001000;
-	DDRB = (DDRB | 0b00100000);
-	
+	DDRD = DDRD | 0b00001000;
+	//Arduino LED
+	DDRB = DDRB | 0b00100000;
 }
-
 
 void loop()
 {
-  
 	blingPattern(sos, sizeof(sos) / sizeof(int), sosTime, sosIndex, sosLedHigh);
-
 }
-
 
 void ledControl(uint8_t status)
 {
 	if (status == HIGH)
 	{
 		//Lue rekisterin bittien tilat ykkösillä AND-operattorilla. 0 nollaa halutun bitin. OR-operaattorilla kijoitettaan haluttu tila nollattuun bittiin.
-		//PORTD = (PORTD & 0b11110111) | 0b1000;
+		PORTD = (PORTD & 0b11110111) | 0b1000;
 		PORTB = (PORTB & 0b11011111) | 0b100000;
 	}
 	else if (status == LOW)
 	{
-		//PORTD = PORTD & 0b11110111;
+		PORTD = PORTD & 0b11110111;
 		PORTB = PORTB & 0b11011111;
 	}
 }
--- EX4/EX4.ino
+++ EX4/EX4.ino
@@ -1,69 +1,97 @@
-
 #define DIFFERENTCHAR 15
 #define D 500 //Char display time
 //													0			1			2			3			4			5			6			7			8			9			.			blank
-const uint8_t sevenNumberBits[DIFFERENTCHAR] = { 0b00111111, 0b00000110, 0b01011011, 0b01001111, 0b01100110, 0b01101101, 0b01111101, 0b01000111, 0b01111111, 0b01101111, 0b10000000, 0b00000000,
-0b01110111, 0b01111001, 0b01110001 };
+const uint8_t sevenSegBits[DIFFERENTCHAR] = { 0b00111111, 0b00000110, 0b01011011, 0b01001111, 0b01100110, 0b01101101, 0b01111101, 0b01000111, 0b01111111, 0b01101111, 0b10000000, 0b00000000,
+												0b01110111, 0b01111001, 0b01110001 };
 //													A			E			F
-const char sevenNumberChars[DIFFERENTCHAR] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', ' ', 'A', 'E', 'F' };
+const char sevenSegChars[DIFFERENTCHAR] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', ' ', 'A', 'E', 'F' };
 
 char wordChar[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', '8', '6', '4', '2', '0', ' ' };
 unsigned int wordDelay[] = { D,   D,   D,   D,   D,   D,   D,   D,   D,   D,   D,   D,   D,   D,   D,   D,   D * 2 };
 unsigned long wordTimeOld = 0;
 unsigned int wordIndex = 0;
 bool wordStart = true;
+
+#define d 300
+char toinen[] =			   { 'A', 'E', 'F', '2', '6', '.' };
+unsigned int toinenAika[] = { d,   d,   d,   d,   d,   d*2};
+unsigned long toinenVanhaAika = 0;
+unsigned int toinenIndeksi = 0;
+bool toinenAloitus = true;
+
 void setup()
 {
+	//Display 1
 	DDRD = 255;
-
+	//Display 2
+	DDRB = 0b1111;
+	DDRC = 0b1111;
 }
-
-
 
 void loop()
 {
-	//sevenSeg('0', false);
-	displayCharacters(wordChar, wordDelay, sizeof(wordChar), wordTimeOld, wordIndex, wordStart);
-
-
+	displayCharacters(wordChar, wordDelay, sizeof(wordChar), wordTimeOld, wordIndex, wordStart, true);
+	displayCharacters(toinen, toinenAika, sizeof(toinen), toinenVanhaAika, toinenIndeksi, toinenAloitus, false);
 }
-
-int sevenSeg(char character, bool decimal)
+//bool dislay: true = PORTD display, false = PORTB + PORTC display
+int sevenSeg(char character, bool decimal, bool display)
 {
 	for (uint8_t i = 0; i < DIFFERENTCHAR; i++)
 	{
-		if (sevenNumberChars[i] == character)
+		if (sevenSegChars[i] == character)
 		{
 			if (decimal == true)
 			{
 				if (character == '.')
 				{
-					PORTD = sevenNumberBits[i];
+					if (display == true)
+					{
+						PORTD = sevenSegBits[i];
+					}
+					else
+					{
+						PORTB = sevenSegBits[i] & 0b00001111;
+						PORTC = (sevenSegBits[i] & 0b11110000) >> 4;
+					}
 				}
 				else
 				{
-					PORTD = sevenNumberBits[i] + 0b10000000;
+					if (display == true)
+					{
+						PORTD = sevenSegBits[i] + 0b10000000;
+					}
+					else
+					{
+						PORTB = sevenSegBits[i] & 0b00001111;
+						PORTC = ((sevenSegBits[i] & 0b11110000) >> 4) + 0b1000;
+					}
 				}
 				return EXIT_SUCCESS;
 			}
 			else
 			{
-				PORTD = sevenNumberBits[i];
-				return EXIT_SUCCESS;
+				if (display == true)
+				{
+					PORTD = sevenSegBits[i];
+				}
+				else
+				{
+					PORTB = sevenSegBits[i] & 0b00001111;
+					PORTC = (sevenSegBits[i] & 0b11110000) >> 4;
+				}
 			}
-
 		}
 	}
 	return EXIT_FAILURE;
 }
 
-void displayCharacters(char word[], unsigned int time[], unsigned int lenght, unsigned long &timeOld, unsigned int &patternIndex, bool &start)
+void displayCharacters(char word[], unsigned int time[], unsigned int lenght, unsigned long &timeOld, unsigned int &patternIndex, bool &start, bool display)
 {
 	if (patternIndex < lenght)
 	{
 		if (patternIndex == 0 && start == true)
 		{
-			sevenSeg(word[patternIndex], false);
+			sevenSeg(word[patternIndex], false, display);
 			start = false;
 			timeOld = millis();
 		}
@@ -72,7 +100,7 @@ void displayCharacters(char word[], unsigned int time[], unsigned int lenght, un
 			if (millis() - timeOld > time[patternIndex])
 			{
 				patternIndex++;
-				sevenSeg(word[patternIndex], false);
+				sevenSeg(word[patternIndex], false, display);
 				timeOld = millis();
 			}
 		}
@@ -82,5 +110,4 @@ void displayCharacters(char word[], unsigned int time[], unsigned int lenght, un
 		patternIndex = 0;
 		start = true;
 	}
-
 }
