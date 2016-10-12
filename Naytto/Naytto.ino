92eab17770edcc1e0dd8227ef72f6e0d874abb01
Matias Saastamoinen <matias.saastamoinen@eng.tamk.fi>
2016-10-12 19:13:52 +0300

    Juu

--- Naytto/Naytto.ino
+++ Naytto/Naytto.ino
@@ -1,23 +1,23 @@
 #include <LiquidCrystal.h>
 
-#define SS0 0
-#define SS1 1
-#define SS2 2
-#define SS3 3
-#define SS4 4
-#define SS5 5
-#define SS6 6
-#define SS7 7
-#define SS8 8
-#define SS9 9
-#define SSA 10
-#define SSB 11
-#define SSC 12
-#define SSD 13
-#define SSE 14
-#define SSF 15
-#define SSdot 16
-#define SSemt 17
+#define K0 0
+#define K1 1
+#define K2 2
+#define K3 3
+#define K4 4
+#define K5 5
+#define K6 6
+#define K7 7
+#define K8 8
+#define K9 9
+#define KA 10
+#define KB 11
+#define KC 12
+#define KD 13
+#define KE 14
+#define KF 15
+#define Kstar 16
+#define Khastag 17
 
 #define keyDelay 5 //ms
 
@@ -32,38 +32,36 @@
 #define columnsLcd 16
 
 #define KEYPADFAILURE -100
-//Näytön sanat						0			1			2			3			4			5			6			7			8			9			.			blank
-const uint8_t sevenSegBits[] = { 0b00111111, 0b00000110, 0b01011011, 0b01001111, 0b01100110, 0b01101101, 0b01111101, 0b01000111, 0b01111111, 0b01101111,
-0b01110111, 0x7C, 0x39, 0x5E, 0b01111001, 0b01110001, 0b10000000, 0b00000000 };
-//									A		B		C	 D	      E			F			.			blank
+
 /*
 const uint8_t keys[4][4] =
-{ { SSA, SS3, SS2, SS1 },
-{ SSB, SS6, SS5, SS4 },
-{ SSC, SS9, SS8, SS7 },
-{ SSD, SSemt, SS0, SSdot }
+{ { KA, K3, K2, K1 },
+{ KB, K6, K5, K4 },
+{ KC, K9, K8, K7 },
+{ KD, Khastag, K0, Kstar }
 };
 */
+
 const char keys[4][4] =
 { { 'A', '3', '2', '1' },
 { 'B', '6', '5', '4' },
 { 'C', '9', '8', '7' },
 { 'D', '#', '0', '*' }
 };
-LiquidCrystal nkn(RSlcd, EnableLcd, data0Lcd, data1Lcd, data2Lcd, data3Lcd);
+
+//nkn = nestekidenäyttö
+LiquidCrystal nkn(RSlcd, EnableLcd, data0Lcd, data1Lcd, data2Lcd, data3Lcd); 
 
 uint8_t i50 = 0;
 const uint8_t maxi50 = 50;
 
-const char chars[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', '.', ' ' };
+const char chars[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', '*', '#' };
 
 int pressedKey = 17;
 int pressedKeyLast = 0;
 
 void setup()
 {
-	//D-portti output
-	//DDRD = 255;
 
 	nkn.begin(columnsLcd, rowsLcd);
 
@@ -105,11 +103,9 @@ int readKeyHit()
 				}
 			}
 			mask2 = mask2 << 1;
-			//mask2 = mask2 | 0b00000001;
 			mask2++;
 		}
 		mask1 = mask1 << 1;
-		//mask1 = mask1 | 0b00000001;
 		mask1++;
 	}
 	return -100;
@@ -131,7 +127,6 @@ int readKeyPress()
 
 void printTo50()
 {
-
 	nkn.clear();
 
 	nkn.setCursor(0, 0);
@@ -210,13 +205,13 @@ int setValue()
 		case '1':
 			value++;
 			break;
-		case 'a':
+		case '4':
 			value--;
 			break;
 		case '3':
 			value+= 10;
 			break;
-		case 'g':
+		case '6':
 			value-= 10;
 			break;
 		case 'D':
@@ -228,71 +223,3 @@ int setValue()
 	}	
 }
 
-int sevenSeg(uint8_t character, bool decimal)
-{
-	if (decimal == true)
-	{
-		if (character == '.')
-		{
-			PORTD = sevenSegBits[character];
-		}
-		else
-		{
-			PORTD = sevenSegBits[character] + 0b10000000;
-		}
-	}
-	else
-	{
-		PORTD = sevenSegBits[character];
-
-	}
-
-	return EXIT_SUCCESS;
-}
-
-void incrementer()
-{
-	int displayChar = displayStatus();
-
-	int sevenChar = readKeyPress();
-
-	if (sevenChar == SS1 || sevenChar == SS3 || sevenChar == SSA)
-	{
-		if (sevenChar == SS1)
-		{
-			displayChar--;
-		}
-		else if (sevenChar == SS3)
-		{
-			displayChar++;
-		}
-		else if (sevenChar == SSA)
-		{
-			displayChar = 5;
-		}
-
-		if (displayChar < SS0)
-		{
-			displayChar = SSF;
-		}
-		else if (displayChar > SSF)
-		{
-			displayChar = SS0;
-		}
-		sevenSeg(displayChar, false);
-	}
-}
-
-int displayStatus()
-{
-	uint8_t temp = PIND;
-
-	for (size_t i = 0; i < sizeof(sevenSegBits); i++)
-	{
-		if (temp == sevenSegBits[i])
-		{
-			return i;
-		}
-	}
-	return -100;
-}
