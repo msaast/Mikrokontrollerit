21df0eda80fe970282dcae4d26c941e268032f92
Matias Saastamoinen <matias.saastamoinen@eng.tamk.fi>
2016-10-26 13:15:38 +0300

    Juu

--- Naytto/Naytto.ino
+++ Naytto/Naytto.ino
@@ -34,156 +34,194 @@
 #define KEYPADFAILURE -100
 
 /*
-const uint8_t keys[4][4] =
-{ { KA, K3, K2, K1 },
-{ KB, K6, K5, K4 },
-{ KC, K9, K8, K7 },
-{ KD, Khastag, K0, Kstar }
-};
-*/
-
 const char keys[4][4] =
 { { 'A', '3', '2', '1' },
 { 'B', '6', '5', '4' },
 { 'C', '9', '8', '7' },
 { 'D', '#', '0', '*' }
 };
+*/
+
+const uint8_t keys[4][4] =
+{ { KA, K3, K2, K1 },
+{ KB, K6, K5, K4 },
+{ KC, K9, K8, K7 },
+{ KD, Khastag, K0, Kstar }
+};
+
+const char chars[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', '*', '#' };
 
 //nkn = nestekidenäyttö
 LiquidCrystal nkn(RSlcd, EnableLcd, data0Lcd, data1Lcd, data2Lcd, data3Lcd); 
 
-uint8_t i50 = 0;
-const uint8_t maxi50 = 50;
-
-const char chars[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', '*', '#' };
-
-int pressedKey = 17;
-int pressedKeyLast = 0;
+int pressedKey = -1;
+int pressedKeyLast = -2;
 
 void setup()
 {
-
 	nkn.begin(columnsLcd, rowsLcd);
 
-	//C-portti input ylös
+	//C-portti input ylösveto
+	//Sarakkeet
 	DDRC = 0b000000000;
 	PORTC = 0b00001111;
 
-	//B-portti output alas
+	//B-portti output
+	//Rivit
 	DDRB = 0b00001111;
 	PORTB = 0b00000000;
-	//Serial.begin(57600);
+	
 }
 
 void loop()
 {
 	aPlusB();
 
-	
+	//setValue();
 	//printTo50();
 }
 
+
 int readKeyHit()
 {
-	uint8_t mask1 = 0b1110;
-	uint8_t mask2 = 0b1110;
+	//Tämä funktio tutkii mikä näppäin on painettuna.
+	//Jos haluaa lukea näppäimistöä, tämän funkiton on pyörittävä jatkuvasti.
+	//Mikrokontrolleri testaa näppäin näppäimeltä onko se painettuna.
 
+	//Alustetaan maskit
+	uint8_t mask1 = 0b1110; //Rivit
+	uint8_t mask2 = 0b1110; //Sarakkeet
+
+	//Ulompi for-looppi käy läpi näppäimistön rivit testateekseen onko jotain sen saraketta painettu.
 	for (uint8_t i = 0; i < 4; i++)
 	{
-		PORTB = mask1 & 0b1111;
-		mask2 = 0b1110;
+		//Asetetaan B-portti rivi-maskin mukaiseksi.
+		//1. kierros 00001110, 2. 00001101, 3. 00001011, 4. 00000111
+		//0 = maa (GND)
+		PORTB = mask1 & 0b1111; 
+		//Kun joku rivi on astuettu maahan, voidaan testata mitä sen rivin saraketta on painettu.
+		//Kaikki rivit käydään erikseen läpi.
+
+		mask2 = 0b1110; //Asetetaan sarakkeiden maski alku asentoon.
+
+		//Sisempi for-looppi tarkistaa mitä saraketta on painettu.
 		for (uint8_t j = 0; j < 4; j++)
 		{
+			//Luetaan sarakkeiden input (C-porttia, neljä vähiten merkitsevää bittiä).
+			//Jos jotain tällä hetkellä testattavan rivin saraketta on painettu.
 			if (0b1111 != (PINC & 0b1111))
 			{
-				delay(keyDelay);
+				delay(keyDelay); //Odotetaan vähän aikaa, että näppäin asettuu.
+
+				// Luetaan näppäimistö uudelleen ja tarkistetaan sarake-maskilla, mitä rivin saraketta on painettu.
 				if ((mask2 & 0b1111) == (PINC & 0b1111))
 				{
+					//Palautetaan kaksiulotteisestataulukosta painetun näppäimen arvo.
+					//Indeksoidaan taulukkukko looppien laskureiden avulla. i on rivi ja j on sarake.
+					//Esim.: Painetaan nappia 5. Se on toisella rivillä (ylhäältä alas) ja kolmannessa sarakkeessa (vasmmalta oikealle).
+					//Joten ulompi for-looppi pitää olla toisella kierroksella (i = 1) ja sisempi kolmannella (j = 2).
+					//Palautetaan keys[1][2], minkä arvo on 5. Loopit jää kesken, koska löydettiin osuma.
 					return keys[i][j];
 				}
 			}
-			mask2 = mask2 << 1;
-			mask2++;
+			//Muokataan sarake-maskia.
+			mask2 = mask2 << 1; //1110 -> 1100
+			mask2++; //1100 -> 1101
+			//Loopin alkuu -> Testataan seuraava sarake.
 		}
-		mask1 = mask1 << 1;
-		mask1++;
+		//Mukataan rivi-maskia.
+		mask1 = mask1 << 1; //1110 -> 1100
+		mask1++; //1100 -> 1101
+		//Loopin alkuu -> Testataan seuraava rivi.
 	}
-	return -100;
+	
+	//Ei osumia, näppäimistöä ei ole painettu. Palatetaan epäonnistuminen.
+	return KEYPADFAILURE;
 }
 
+//Tämä funktio palauttaa näppäimen arvon, kun näppäin päästetään ylös.
 int readKeyPress()
 {
-	int temp = readKeyHit();
+	int temp = readKeyHit(); //Luetaan mikä näppäin on painettuna väliaikais muuttujaan.
+	//Jos joku näppain on painettuna.
 	if (temp >= 0)
 	{
-		while (temp == readKeyHit())
+		//Pyöritetään looppia niin kauvan kuin sama nappi on painettuna.
+		while (temp == readKeyHit()) {}
+		//Nappi päästettiin ylös.
+		delay(keyDelay); //Odotetaan, että nappi asettuu.
+
+		//Tarkistetaan uudelleen, että nappi on ylhäällä.
+		if (readKeyHit() == KEYPADFAILURE)
 		{
+			//Nappi päästetty ylös onnistuneesti. Palautetaan pohjassa olleen napin arvo.
+			return temp;
 		}
-		delay(keyDelay);
-		return temp;
 	}
-	return -100;
+	//Näppäimistöä ei ole painettu. Palautetaan epäonnistuminen.
+	return KEYPADFAILURE;
 }
 
 void printTo50()
 {
-	nkn.clear();
-
-	nkn.setCursor(0, 0);
-	nkn.print(i50);
-	nkn.setCursor(0, 1);
-	nkn.print(sqrt(i50), 3);
-
-	nkn.setCursor(columnsLcd - 2, 0);
-	nkn.print(maxi50 - i50);
-	nkn.setCursor(columnsLcd - 3, 1);
-	nkn.print(-(maxi50 - i50));
-	i50++;
-
-	if (i50 > maxi50)
+	const uint8_t iMax = 50;
+	for (uint8_t i = 0; i < iMax; i++)
 	{
-		i50 = 0;
-	}
-	delay(400);
+		nkn.clear();
 
+		nkn.setCursor(0, 0);
+		nkn.print(i);
+		nkn.setCursor(0, 1);
+		nkn.print(sqrt(i), 3);
+
+		nkn.setCursor(columnsLcd - 2, 0);
+		nkn.print(iMax - i);
+		nkn.setCursor(columnsLcd - 3, 1);
+		nkn.print(-(iMax - i));
+		
+		delay(400);
+	}
 }
 
 void displayKeyLcd()
 {
-	pressedKey = readKeyPress();
+	waitInput();
 
-	if (pressedKey != pressedKeyLast && pressedKey != -100)
+	if (pressedKey != pressedKeyLast )
 	{
 		nkn.clear();
 		nkn.setCursor(columnsLcd / 2 - 1, 0);
-		nkn.print(char(pressedKey));
+		nkn.print(chars[pressedKey]);
 	}
 }
 
 void aPlusB()
 {
+	#define firstChar 4
+	#define row 0
 	nkn.clear();
-	nkn.setCursor(4, 0);
-	nkn.print(" + =");
+	nkn.setCursor(firstChar, row);
+	const char line[] = { " + =" };
+	nkn.print(line);
 	int temp0, temp1;
-	while ((pressedKey = readKeyPress()) == -100) {}
+
+	waitInput();
 
 	temp0 = pressedKey;
-	nkn.setCursor(4, 0);
-	nkn.print(char(temp0));
+	nkn.setCursor(firstChar, row);
+	nkn.print(chars[temp0]);
 
-	while ((pressedKey = readKeyPress()) == -100) {}
+	waitInput();
 
 	temp1 = pressedKey;
 
-	nkn.setCursor(6, 0);
-	nkn.print(char(temp1));
-	nkn.setCursor(8, 0);
+	nkn.setCursor(firstChar + 2, row);
+	nkn.print(chars[temp1]);
+	nkn.setCursor(firstChar + sizeof(line) - 1, row);
 
+	nkn.print(temp0 + temp1);
 
-	nkn.print((temp0 - '0') + (temp1 - '0'));
-
-	while ((pressedKey = readKeyPress()) == -100) {}
+	waitInput();
 }
 
 int setValue()
@@ -198,9 +236,9 @@ int setValue()
 		nkn.setCursor(sizeof(line) + 1, 0);
 		nkn.print(value);
 
-		while ((pressedKey = readKeyPress()) == KEYPADFAILURE) {}
+		waitInput();
 
-		switch (pressedKey)
+		switch (chars[pressedKey])
 		{
 		case '1':
 			value++;
@@ -223,3 +261,12 @@ int setValue()
 	}	
 }
 
+//Odotetaan, että näppäimistö painetaan.
+void waitInput()
+{
+	//Sijotetaan globaliin "pressedKey"-muuttajaan syöte näppäimitöltä.
+	//Looppi pyörii, kun näpäimistön luentafunktio palauttaa epäonnistumisen (näppäimistöä ei ole painettu).
+	while ((pressedKey = readKeyPress()) == KEYPADFAILURE) {}
+	//Kun funktio palauttaa jotain muuta kuin epäonnistumisen, looppi loppuu ja päästään eteen päin.
+	//"pressedKey"-muuttaja sisältää painetun napin.
+}
